7e974f666a3213d59f4ac6dd54bc06dfef010d7e
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-25 22:48:31 +0700

    oke gas
    set lokasi views agar tidak nyasar

--- xpress-ejs/index.js
+++ xpress-ejs/index.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 8080;
+app.set('view engine', 'ejs'); //require ejs langsung ke express beda dg module yg lain
+
+app.set('views', path.join(__dirname, '/views'));
+console.log(path.join(__dirname, '/views'));
+ app.get('/',(req,res)=>{ //set route ke root
+     res.render('home')
+ });
 
 app.listen(port,()=>{
-    app.set('view engine', 'ejs'); //require ejs langsung ke express beda dg module yg lain
-
-    app.get('/',(req,res)=>{ //set route ke root
-        res.render('home')
-    })
     console.log('listening on port htp://localhost:8080');
-})
+});
